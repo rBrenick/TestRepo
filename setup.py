115246aab54d9b24c97eaf7081891f6472fe5242
Richard Brenick <RichardBrenick@gmail.com>
2019-06-06 01:18:13 +0200

    setup attempt data files

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rBrenick/TestRepo",
     packages=setuptools.find_packages(),
+    data_files=[('data', ['.'])],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
